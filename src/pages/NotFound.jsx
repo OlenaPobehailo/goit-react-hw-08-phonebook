3f0e90b3b7b1ba4c3ce02863c8d1cbf5b5ce3187
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-12 23:52:23 +0200

    updates NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+
 const NotFound = () => {
-  return <div className="container">NotFound</div>;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return (
+    <div className="container center">
+      <p>Page Not Found</p>
+      {isLoggedIn ? (
+        <Link to="/contacts">Contacts</Link>
+      ) : (
+        <Link to="/">Home page</Link>
+      )}
+    </div>
+  );
 };
 
 export default NotFound;
